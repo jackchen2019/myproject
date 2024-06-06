5d39ec790b6c9dce554a1eacd29666f6451b2843
jackchen2019 <chenkui0995@163.com>
2024-06-06 14:42:46 +0800

    123

--- src/main/java/com/itshanghai/myproject/controller/TestHello.java
+++ src/main/java/com/itshanghai/myproject/controller/TestHello.java
@@ -6,7 +6,7 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class TestHello {
 
-    @RequestMapping("hello1")
+    @RequestMapping("hello")
     public String test(){
         return "hello world";
     }
